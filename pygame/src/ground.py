25f14c52098bef2cf5a6da4e45e3ed634394c4a2
Roman Faustov <romanf@assuta.co.il>
2023-06-28 15:12:51 +0300

    random sprite for ground added

--- pygame/src/ground.py
+++ pygame/src/ground.py
@@ -1,10 +1,11 @@
 from pygame.sprite import Group, Sprite
 from .object import Object
+import random
 class Platform(Sprite, Object):
-    plat_img_1 = "assets/Terrain1.png"
-    plat_img_2 = "assets/Terrain2.png"
-    
+    # plat_img_1 = "assets/Terrain1.png"
+    # plat_img_2 = "assets/Terrain2.png"
+    plat_imgs = ["assets/Terrain1.png", "assets/Terrain2.png"]
     def __init__(self, x, y, width, height):
         super().__init__()
-        Object.__init__(self, x, y, width, height, self.plat_img_1)
+        Object.__init__(self, x, y, width, height, random.choice(self.plat_imgs))
 
